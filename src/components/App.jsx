1e89c50d71b7cdb4a1c71b5ff27810a9fb54368e
navneet-patwal <navneet.patwa31@mailcom>
2022-02-09 19:08:46 +0530

    project complete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,103 @@
-import React from "react";
-import { ReactDOM } from "react";
 
-function Heading(){
-    return <h1>hello world</h1>
+import React,{useState} from "react";
+
+function App() {
+
+    //states for updating ui 
+
+const [result, setResult] = useState("");
+const [scifiButton, setButton] = useState("");
+const [isToggled, setToggle] =useState(false);
+const [theme,setTheme] = useState(false);
+const [buttonClass,setClass] = useState("button");
+const [calcClass,setCalc] = useState("calculator");
+
+
+//creating buttons
+const operators = ["/","*","+","-"];
+const display = [1,2,3,4,5,6,7,8,9,0,...operators];
+const digitsButton =[];
+
+for(let i = 0; i < display.length; i++){
+    digitsButton.push(
+    <button className={buttonClass} value={display[i]} key={i} onClick={displayValue}>{display[i].toString()}</button>
+    );
 }
 
-export default Heading;
+//showing expression on Screen
+function displayValue (event)
+{
+    setResult(prevValue => prevValue+(event.target.value).toString());
+    
+}
+
+//calculating expression
+function calculate() 
+{
+    const value = document.getElementsByClassName("screen")[0].innerText;
+    for(let k =0; k < value.length; k++){
+        let exp += eval(value[k-1]+value[k]+value[k+1]);
+    }
+    setResult(exp);
+    return value;
+}
+
+
+//calculating squareroot
+ function squareRoot() 
+{
+      Math.sign(calculate()) === -1? setResult("Error") :setResult(calculate()*calculate());
+}
+
+
+//sign of number
+function sign()
+{
+    Math.sign(calculate()) === -1? setResult(Math.abs(calculate())) : setResult("-"+calculate().toString());    
+}
+
+
+//scientificMode
+function scifiMode()
+{
+
+    isToggled ?  setButton("") :
+    setButton(
+    <div>
+           <button className={buttonClass} onClick={()=>{setResult(calculate()*calculate());}}>square</button>
+           <button className={buttonClass} onClick={squareRoot}>sq.Root</button>
+           <button className={buttonClass} onClick={sign}>+/-</button>
+    </div>) ;
+
+    setToggle((rev) => !rev);
+    }
+
+//Changing theme between light and dark
+function changeTheme(){
+    if(theme){
+      setClass("button dark");
+      setCalc("calculator dark");
+
+    }else {
+        setClass("button");
+        setCalc("calculator");
+    }
+    setTheme((prev)=> !prev);
+}
+
+
+return ( 
+<div className={calcClass}>
+<div className={buttonClass}>{result}</div>
+    <div>{digitsButton}
+        <button  className={buttonClass} onClick={()=> setResult("")}>clear</button>
+        <button  className={buttonClass} onClick={calculate}>=</button>
+        <button  className={buttonClass} onClick={scifiMode} >SciFI</button>
+        <button  className={buttonClass} onClick={changeTheme}>theme</button>
+        {scifiButton}
+    </div>
+</div>
+);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Heading from "./components/App"
+import App from "./components/App";
+
+
+
 
 ReactDOM.render(
-    ,
+   <App /> ,
   document.getElementById('root')
 );
 
